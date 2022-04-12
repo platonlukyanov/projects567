f6c3bf5efaaf6d5d4f76a5ffbe38dbb1c98776cf
root <root@v1049043.hosted-by-vdsina.ru>
2022-04-12 16:20:03 +0300

    secret file according to env

---
+++ secret.py
@@ -0,0 +1,8 @@
+import os
+
+secret_key = os.getenv("SECRET")
+conf_salt = os.getenv("SALT")
+
+m_password = os.getenv("MAIL_PASSWORD")
+m_username = os.getenv("MAIL_USER")
+m_default_sender = os.getenv("MAIL_DEFAULT_SENDER")
